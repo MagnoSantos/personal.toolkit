808f5de9773adf5d1879f2e805335d6bf004534f
Magno Juliano Goncalves Santos <magno.santos@neon.com.br>
2022-01-05 16:01:03 -0300

    feat: sample health check and tests

---
+++ Personal.Infra/HealthCheck/ExampleHealthCheck.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Personal.Infra.HealthCheck
+{
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        private readonly bool _healthCheckResultHealthy;
+
+        public ExampleHealthCheck(bool healthCheckResultHealthy)
+        {
+            _healthCheckResultHealthy = healthCheckResultHealthy;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            if (_healthCheckResultHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, "An unhealthy result."));
+        }
+    }
+}
---
+++ Personal.UnitTests/Infra/ExampleHealthCheckTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Personal.Infra.HealthCheck;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Personal.UnitTests.Infra
+{
+    public class ExampleHealthCheckTests
+    {
+        [Fact]
+        public async Task CheckHealthAsync_Expecting_Healthy()
+        {
+            var health = new ExampleHealthCheck(true);
+
+            var result = await health.CheckHealthAsync(new HealthCheckContext(), default);
+
+            result.Status.Should().Be(HealthStatus.Healthy);
+        }
+    }
+}

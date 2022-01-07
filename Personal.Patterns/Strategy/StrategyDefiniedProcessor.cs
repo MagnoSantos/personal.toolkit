266f85d924fb2d1c1ba8e7e52e3bea5c5d2a59cc
Magno Juliano Goncalves Santos <magno.santos@neon.com.br>
2022-01-07 15:11:28 -0300

    tess: definied processor

---
+++ Personal.Patterns/Strategy/StrategyDefiniedProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Personal.Patterns.Strategy
+{
+    public class StrategyDefiniedProcessor
+    {
+        public string Process(Type typeToProcess)
+        {
+            return typeToProcess switch
+            {
+                Type.StrategyA => new StrategyA().ExecuteAsync(),
+                Type.StrategyB => new StrategyB().ExecuteAsync(),
+                _ => throw new System.NotImplementedException(),
+            };
+        }
+    }
+
+    public enum Type
+    {
+        StrategyA, 
+        StrategyB
+    }
+}
---
+++ Personal.UnitTests/Patterns/Strategy/StrategyDefiniedProcessorTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Personal.Patterns.Strategy;
+using Xunit;
+
+namespace Personal.UnitTests.Patterns.Strategy
+{
+    public class StrategyDefiniedProcessorTests
+    {
+        [Fact]
+        public void StrategyA_Expecting_Successfull()
+        {
+            var strategyProcessor = new StrategyDefiniedProcessor();
+
+            var result = strategyProcessor.Process(Type.StrategyA);
+
+            result.Should().Be("Strategy A processed");
+        }
+
+        [Fact]
+        public void StrategyB_Expecting_Successfull()
+        {
+            var strategyProcessor = new StrategyDefiniedProcessor();
+
+            var result = strategyProcessor.Process(Type.StrategyB);
+
+            result.Should().Be("Strategy B processed");
+        }
+    }
+}

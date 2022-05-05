63df26711d6501de37f20cf4e16cf490f9140751
Magno Santos <magno.ufvjm@gmail.com>
2022-05-05 13:38:12 -0300

    feat: custom exceptions

---
+++ Personal.Commom/CustomException.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Personal.Commom
+{
+    public class CustomException : Exception
+    {
+        public CustomException() : base()
+        {
+        }
+
+        public CustomException(string? message) : base(message)
+        {
+        }
+
+        public CustomException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Personal.UnitTests/Commom/CustomExceptionTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Personal.Commom;
+using System;
+using Xunit;
+
+namespace Personal.UnitTests.Commom
+{
+    public class CustomExceptionTests
+    {
+        [Fact]
+        public void CustomExceptionBase()
+        {
+            var customException = new CustomException();
+
+            customException.Message.Should()
+                .Be("Exception of type 'Personal.Commom.CustomException' was thrown.");
+        }
+
+        [Fact]
+        public void CustomExceptionWithMessage()
+        {
+            var customException = new CustomException("Error");
+
+            customException.Message.Should().Be("Error");
+        }
+
+        [Fact]
+        public void CustomExceptionWithInnerException()
+        {
+            var exception = new Exception();
+
+            var customException = new CustomException("Error", exception);
+
+            customException.Message.Should().Be("Error");
+            customException.InnerException.Should().Be(exception);
+        }
+    }
+}
